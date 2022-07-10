4fedfcb3a4e965b530305b207ad82e3d564b0954
UnInformaticoAburrido <dimitrycompanycifre@gmail.com>
2022-07-10 18:37:03 +0200

    carga2-10/07/2022 Trabajando en el objeto producto.

---
+++ AlbaCash/src/main/java/com/dimitry/albacash/objects/Producte.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dimitry.albacash.objects;
+
+import com.dimitry.albacash.utilities.*;
+
+/**
+ *
+ * @author didra
+ */
+public class Producte {
+    private String nombre;
+    private Alergenos[] listadoAlergenos;
+    private int stock;
+    private Producte[] ingredientes;
+    //Generamos un constructor vacio que se usara para generar recursivamente productos desde el archivo
+    public Producte() {
+    }
+    //Constructor base este se eliminara en un futuro
+
+    public Producte(String nombre, Alergenos[] listadoAlergenos, int stock, Producte[] ingredientes) {
+        this.nombre = nombre;
+        this.listadoAlergenos = listadoAlergenos;
+        this.stock = stock;
+        this.ingredientes = ingredientes;
+    }
+    
+    
+}
