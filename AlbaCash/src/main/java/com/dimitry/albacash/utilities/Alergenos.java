d3efdbab41e1f273045f821fdf36c310b3f5d8cf
UnInformaticoAburrido <dimitrycompanycifre@gmail.com>
2022-07-10 17:50:53 +0200

    carga2-10/07/2022

---
+++ AlbaCash/src/main/java/com/dimitry/albacash/utilities/Alergenos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.dimitry.albacash.utilities;
+
+/**
+ *
+ * @author didra
+ */
+public enum Alergenos {
+    CEREALES, FRUTOSsECOS, MARISCOS, CRUSTACEOS, HUEVOS, PESCADO, CACAHUETES, SOJA, LECHE, SESAMO, APIO, MOSTAZA, ALTRAMUCES, SULFITOS;
+}
